8a1aafc39b8c942b7bf129cfe22089a50466ca6f
nexus122 <juan-pablo122@hotmail.es>
2022-03-28 10:20:56 +0200

    Ejercicio Solucionado

--- app.js
+++ app.js
@@ -1,32 +1,61 @@
-//generate random numbers
-let firstNumber = parseInt(Math.random()*10);
-let secondNumber = parseInt(Math.random()*10);
+// Estado de la aplicación.
+let state = {
+    primaryNumber: $('#primary-number'),
+    secondaryNumber: $('#secondary-number'),
+    resultado: $('#guess'),
+    button: $('#btn'),
+    alert: $('.alert'),
+    correctAnswer: 0,    
+}
 
-//get the total
-let total = firstNumber + secondNumber;
+// Generamos dos numeros aleatorios y generamos una respuesta a la suma de esos numeros.
+function generateRandom(){
+    let random1 = Math.floor(Math.random() * 10 + 1);
+    let random2 = Math.floor(Math.random() * 10 + 1);
+    
+    state.primaryNumber.text(random1);
+    state.secondaryNumber.text(random2);
 
-//display numbers on the canvas
-let primary = document.getElementById('primary-number');
-    primary.innerHTML = `<p>${firstNumber}</p>`;
+    state.correctAnswer = random1 + random2;
 
-let secondary = document.getElementById('secondary-number');
-    secondary.innerHTML = `<p>${secondNumber}</p>`
-
-
-//get guess from user
-let button = document.getElementById('btn')
-
-button.addEventListener('click', function(){
-
-let guess = document.getElementById('guess').value;
-    guess = Number(guess);
-//check answer
-if (guess === total){
-    alert('Correct');
-    window.location.reload()
-} else {
-    alert('Sorry. Incorrect. The correct answer was ' + total + '.')
-    window.location.reload()
+    // Ocultamos el color del input
+    state.resultado.removeClass("invalid");
+    state.resultado.removeClass("valid"); 
 
 }
-    });
+
+// Mensaje que aparece y desaparece.
+function toast(message, valid){
+    state.alert.show(1000);
+    state.alert.html(message);
+
+    if(valid){
+        state.alert.removeClass("invalid");
+        state.alert.addClass("valid");
+    }else{
+        state.alert.removeClass("valid");
+        state.alert.addClass("invalid");
+    }
+
+    setTimeout(function(){state.alert.hide(1000)}, 1500);
+
+}
+
+// Controlamos que el usuario haga click en el btn check.
+state.button.click(function(){
+    if(state.resultado.val() == state.correctAnswer){
+        state.resultado.removeClass("invalid")
+        state.resultado.addClass("valid");   
+        toast(`👍 El resultado es correcto`, true);     
+        generateRandom();
+    }else{
+        state.resultado.removeClass("valid")
+        state.resultado.addClass("invalid");
+        toast(`👎 El resultado es incorrecto`, false);
+    }
+
+})
+
+
+// Generamos un numero aleatorio al inicio
+generateRandom();
